62ede90a003b30b05d46ea5b02ea71868efc623f
Rob Bishop <c.sagan.ritual@gmail.com>
2021-12-10 14:27:50 -0800

    New screenshot, just for fun

--- Shared/AppModel.swift
+++ Shared/AppModel.swift
@@ -15,13 +15,13 @@ class AppModel: ObservableObject {
             newTumbler.rollMode.value = .normal
 
             if tumblerType == .outerRing {
-                newTumbler.radius.value = 1.0
+                newTumbler.radius.value = 0.125
                 newTumbler.pen.value = 0.0    // Future feature, pen in the outermost ring?
             }
 
             if tumblerType == .innerRing {
-                newTumbler.radius.value = 0.625
-                newTumbler.pen.value = 0.0375
+                newTumbler.radius.value = 0.5
+                newTumbler.pen.value = 1.0
             }
 
             return newTumbler
